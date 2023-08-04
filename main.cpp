2e374ed9bda261e758c3b3c083786834e4c404b3
Sandip Katel <109036168+sandipkatel@users.noreply.github.com>
2023-08-04 16:49:37 +0545

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 /* Play Sudoku */
+#include <string>
+char username[] = "username: sandip"; //Dummy username
+char bestscore[] = "Best time: 00:22:23"; //just a format
+char bestgammer[] = "By: katel";  //just a format
 #include "Sudoku.h"
 
 int main(int argc, char* argv[])
