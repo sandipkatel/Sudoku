fc50ff3d8c923fbfd2afe2be3c7cb2e9f8e9f648
Sandip Katel <109036168+sandipkatel@users.noreply.github.com>
2023-08-04 16:48:34 +0545

    Add files via upload

--- Sudoku.h
+++ Sudoku.h
@@ -222,51 +222,44 @@ void Sudoku::Sudoku::preloadTextures()
 	loadTexture(mTextureCache[11], "New", fontColour);
 	loadTexture(mTextureCache[12], "Wrong!", fontColour);
 	loadTexture(mTextureCache[13], "Right!", fontColour);
-	loadTexture(mTextureCache[14], "Hint", fontColour);
-	loadTexture(mTextureCache[15], "SUDOKU", fontColour);
-	loadTexture(mTextureCache[16], "Exit", fontColour);
-	loadTexture(mTextureCache[17], "Username", fontColour);
-	loadTexture(mTextureCache[18], "Best Score", fontColour);
-	
+	loadTexture(mTextureCache[14], "SUDOKU", fontColour);
+	loadTexture(mTextureCache[15], "Exit", fontColour);
+	loadTexture(mTextureCache[16], username, fontColour);
+	loadTexture(mTextureCache[17], bestscore, fontColour);
+	loadTexture(mTextureCache[18], bestgammer, fontColour);
+	loadTexture(mTextureCache[19], "Hint", fontColour);
 }
 
 void Sudoku::Sudoku::createInterfaceLayout()
 {
-	// Define thick and thin borders
-    const int thinBorder = 2;
-    const int thickBorder = thinBorder + 6;
-
-	//Define start state
-	int buttonStartRow = 0;  // s down
-	int buttonWidth = mGridWidth - 2 * thickBorder;   //s just timers button width
-	// mWindowHeight = buttonHeight + 6 * thinBorder + 6 * thickBorder (rearange this equation)
-	int buttonHeight = (mWindowHeight - 6 * thinBorder - 6 * thickBorder-30) / 11;  //s cells height
-	int buttonStartCol = 0;
-	
 	// Set render color to light green (RGB: 153, 209, 213)
 	SDL_SetRenderDrawColor(mRenderer, 153, 209, 213, SDL_ALPHA_OPAQUE);
 	// Clear the screen with light green color
 	SDL_RenderClear(mRenderer);
 
-	bool fill = 1, draw = 0;  // to draw or fill
-	//display the background of Sudoku black
+	bool fill = 1, draw = 0;  // to draw or fill the rectangle
+	//display the background of Sudoku white
 	SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, 255);
-	mRectangle.renderRectangle(mRenderer,0, 0, mGridWidth, mGridHeight, fill);
+	SDL_Rect buttonRect = {0, 0, mGridWidth, mGridHeight};
+	mRectangle.renderRectangle(mRenderer,buttonRect, fill);
 
 
-	// s distance from top
-	buttonStartRow += thickBorder;
-	buttonStartCol += thickBorder;
-	// Set button position and dimensions
-	SDL_Rect buttonRect = { buttonStartCol, buttonStartRow, buttonWidth, buttonHeight };
-	mTimer.setButtonRect(buttonRect);
 	/*----------------------------------Create buttons for each cell-------------------------------------*/
+	// Define thick and thin borders
+    const int thinBorder = 2;
+    const int thickBorder = thinBorder + 6;
+
 	// Define cell button dimensions
 	// mGridWidth = 6 * thinBorder + 4 * thickBorder + 9 * buttonWidth (rearrange this equation)
-	buttonWidth = (mGridWidth - 6 * thinBorder - 4 * thickBorder) / mGridCols;
+	int buttonWidth = (mGridWidth - 6 * thinBorder - 4 * thickBorder) / mGridCols;
+	int buttonHeight = buttonWidth-thickBorder;
 
+	//Define start state
+	int buttonStartRow = 0; 
+	int buttonStartCol = 0;
+	
 	// Carry on from previous starting row
-	buttonStartRow += buttonHeight;
+	//buttonStartRow += buttonHeight;
 
 	// Set cell button position and dimensions
 	for (int gridRow = 0; gridRow < mGridRows; gridRow++)
@@ -293,35 +286,84 @@ void Sudoku::Sudoku::createInterfaceLayout()
 		}
 	}
 
+	//Right side
+	int rightX = mGridWidth + buttonHeight;
+	int rightY = buttonHeight;
+	int width  = 8;
+	int height = mGridHeight-2*buttonHeight;
+
+	//Display the seperator line
+	SDL_SetRenderDrawColor(mRenderer, 0, 0, 0, 255);
+	buttonRect = {rightX, rightY, width, height};
+	mRectangle.renderRectangle(mRenderer,buttonRect, fill);
+
+	rightX += width;
+	buttonWidth = (WindowWidth-rightX);
+	buttonHeight/=1.5;
+	rightY = thickBorder;
+	//Display Sudoku
+	buttonRect = {rightX, rightY, buttonWidth, buttonHeight};
+	mTexts.setButtonRect(buttonRect);
+	mTexts.setTexture(mTextureCache[14]);
+	mTexts.centerTextureRect(1.5);
+	mTexts.renderTexture(mRenderer);
+
+	//Display Sudoku
+	buttonRect = {rightX + buttonWidth - buttonHeight, rightY, buttonHeight, buttonHeight/2};
+	mTexts.setButtonRect(buttonRect);
+	mTexts.setTexture(mTextureCache[15]);
+	mTexts.centerTextureRect(2.5);
+	mTexts.renderTexture(mRenderer);
+	mTexts.renderRectangle(mRenderer, buttonRect, draw);
+
+	//set username
+	rightX += buttonHeight;
+	rightY +=buttonHeight;
+	buttonRect = {rightX, rightY, buttonWidth, buttonHeight};
+	mTexts.setButtonRect(buttonRect);
+	mTexts.setTexture(mTextureCache[16]);
+	mTexts.centerTextureRect(1.5, 'n');
+	mTexts.renderTexture(mRenderer);
+
+	//set timer rect
+	rightY +=buttonHeight;
+	buttonRect = {rightX, rightY, buttonWidth, buttonHeight};
+	mTimer.setButtonRect(buttonRect);
+
+	//display best score and best scorer
+	rightY +=buttonHeight;
+	buttonRect = {rightX, rightY, buttonWidth, buttonHeight};
+	mTexts.setButtonRect(buttonRect);
+	mTexts.setTexture(mTextureCache[17]);
+	mTexts.centerTextureRect(2);
+	mTexts.renderTexture(mRenderer);
+
+	rightY +=buttonHeight/1.5;
+	buttonRect = {rightX, rightY, buttonWidth, buttonHeight};
+	mTexts.setButtonRect(buttonRect);
+	mTexts.setTexture(mTextureCache[18]);
+	mTexts.centerTextureRect(2);
+	mTexts.renderTexture(mRenderer);
+
+
 	/*----------------------------------Create check, and new buttons-------------------------------------*/
 	const int numberOfOtherButtons = 2;
 	mCheckButton.setTexture(mTextureCache[10]);
 	mNewButton.setTexture(mTextureCache[11]);
 	Button* otherButtons[numberOfOtherButtons] = { &mCheckButton, &mNewButton };
 
-	// Redefine button width
-	// mGridWidth = 3 * thickBorder + 9 * numberOfOtherButtons (rearrange this equation)
-	buttonWidth = (mGridWidth - 3 * thickBorder) / numberOfOtherButtons;
-
-	// Carry on from previous starting row
-	buttonStartRow += buttonHeight + thickBorder;
-
-	// Reset starting column
-	int borderWidthTotal = 0;
-
 	// Set check, solve, and new buttons (last row)
+	buttonWidth/=2;
+	rightX = rightX + ((WindowWidth - rightX) - buttonWidth)/2;
 	for (int button = 0; button < numberOfOtherButtons; button++) // colBlock is every 3 columns of cells
 	{
-		// Add border width to total
-		if (button == 0) borderWidthTotal += thickBorder;
-		else borderWidthTotal += thickBorder;
-		int buttonStartCol = button * buttonWidth + borderWidthTotal;
-
+		rightY+=buttonHeight +thickBorder;
 		// Set button position and dimensions
-		SDL_Rect buttonRect = { buttonStartCol, buttonStartRow, buttonWidth, buttonHeight };
+		SDL_Rect buttonRect = { rightX-1, rightY-1, buttonWidth+2, buttonHeight+2};
+		otherButtons[button]->renderRectangle(mRenderer,buttonRect, draw);  //mantanance is required to make both draw and display
+		buttonRect = { rightX, rightY, buttonWidth, buttonHeight};
 		otherButtons[button]->setButtonRect(buttonRect);
 	}
-
 }
 
 void Sudoku::Sudoku::generateSudoku()
@@ -608,7 +650,7 @@ void Sudoku::Sudoku::play()
 		timerStream << std::setfill('0') << std::setw(2) << hours << ":"
 					<< std::setfill('0') << std::setw(2) << minutes << ":"
 					<< std::setfill('0') << std::setw(2) << seconds;
-		std::string timerStr = "Time Elapshed " + timerStream.str();
+		std::string timerStr = "Time elapshed " + timerStream.str();
 
 		// Load and render timer (TO DO: use preloaded textures to render timer)
 		SDL_Texture* timerTexture = nullptr;
@@ -617,7 +659,7 @@ void Sudoku::Sudoku::play()
 		//loadTexture(timerTexture, timer, fontColour);
 		mTimer.setTexture(timerTexture);
 		mTimer.renderButton(mRenderer);
-		mTimer.centerTextureRect();
+		mTimer.centerTextureRect(1.5, 'n');
 		mTimer.renderTexture(mRenderer);
 		SDL_DestroyTexture(timerTexture);
 		timerTexture = nullptr;
