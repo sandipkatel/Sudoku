cfed2c732be0d490e639e4df8772db0401f94c70
Sandip Katel <109036168+sandipkatel@users.noreply.github.com>
2023-08-23 14:17:45 +0545

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//main.cpp
+/* Play sudoku  have fun*/
+#include "GameLoop.h"
+
+int main(int argc, char* argv[])
+{
+	// Create Sudoku game object
+	Sudoku::Sudoku S;
+
+	//Start the Sudoku
+	S.display();
+
+	return 0;
+}
